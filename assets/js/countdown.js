d9105dde46a9e3d8a4979e6c8ff2e55cf88bf2b8
Michael Mateo <michaelallen.mateo@gmail.com>
2018-04-19 18:04:40 +0800

    Capstone-1 9th commit

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,6 +1,6 @@
 function makeTimer() {
 
-			var endTime = new Date("16 April 2018 18:00:00");			
+			var endTime = new Date("23 April 2018 18:00:00");			
 			endTime = (Date.parse(endTime) / 1000);
 
 			var now = new Date();
